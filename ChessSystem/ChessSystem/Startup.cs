b4c9e8e8aa661baa2bfa5e6a00e90cc306db438d
Radosław Groński <gronski.radek@gmail.com>
2017-05-30 00:13:07 +0200

    Styles for login view.

--- ChessSystem/ChessSystem/Startup.cs
+++ ChessSystem/ChessSystem/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Owin;
+﻿using System.Web.Security;
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(ChessSystem.Startup))]
@@ -9,6 +10,7 @@ namespace ChessSystem
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            Membership.ApplicationName = "Chess System";
         }
     }
 }
